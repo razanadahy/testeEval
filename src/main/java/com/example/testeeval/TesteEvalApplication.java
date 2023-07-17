2dcd6c4a42d38e63b73b3dece6b7ff969e543ab9
razanadahy <andrianiavo.vit@gmail.com>
2023-07-17 08:11:09 +0300

    ity

---
+++ src/main/java/com/example/testeeval/TesteEvalApplication.java
@@ -0,0 +1,13 @@
+package com.example.testeeval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteEvalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteEvalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testeeval/TesteEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeeval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteEvalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
